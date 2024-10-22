802fe5c180b91ba511c06a221bf3cdbda552fed2
svolp <143809611+svolp@users.noreply.github.com>
2024-10-22 09:30:58 -0400

    Add files via upload

---
+++ AngryBear.java
@@ -0,0 +1,20 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+
+public class AngryBear 
+{
+    //add instance variables
+    //one for number of teeth
+    //one for number of days awake
+    
+    public AngryBear( int one, int two )
+    {
+    	//add code here
+    }
+    
+    public boolean isAngry()
+    {
+    	//add code here
+    	return false;
+    }
+}
