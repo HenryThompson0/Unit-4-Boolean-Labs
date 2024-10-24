c9da343bb60acb123ffd59898a39ea571545aeb3
svolp <143809611+svolp@users.noreply.github.com>
2024-10-24 09:09:09 -0400

    Add files via upload

---
+++ RockPaperScissors.java
@@ -0,0 +1,28 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name -  
+
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class RockPaperScissors
+{
+	private String playChoice;
+	private String compChoice;
+
+	public RockPaperScissors(String player)
+	{
+	}
+
+	public String determineWinner()
+	{
+		String winner="";
+		return winner;
+	}
+
+	public String toString()
+	{
+		String output="";
+		return output;
+	}
+}
