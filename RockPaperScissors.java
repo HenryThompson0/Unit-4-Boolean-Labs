81e664202fa10b91994e6c40b1c60a7ad27a8209
HenryThompson0 <sayjokesphotos@gmail.com>
2024-10-28 09:27:51 -0400

    Update RockPaperScissors.java

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -2,27 +2,88 @@
 // www.apluscompsci.com
 //Name -  
 
-import java.util.Scanner;
-import static java.lang.System.*;
-
 public class RockPaperScissors
 {
 	private String playChoice;
 	private String compChoice;
+    private int compNumber;
 
 	public RockPaperScissors(String player)
 	{
+        playChoice = player;
+        compNumber = (int)((Math.random()*3)+1);
+        if (compNumber==1)
+        {
+            compChoice="R";
+        }
+        else if (compNumber==2)
+        {
+            compChoice="P";
+        }
+        else if (compNumber==3)
+        {
+            compChoice="S";
+        }
 	}
 
 	public String determineWinner()
 	{
-		String winner="";
+        String winner="";
+
+        if (playChoice.equals("R"))
+        {
+            if (compChoice.equals("R"))
+            {
+                winner="tie";
+            }
+            if (compChoice.equals("P"))
+            {
+                winner="computer wins";
+            }
+            if (compChoice.equals("S"))
+            {
+                winner="player wins";
+            }
+        }
+
+        if (playChoice.equals("P"))
+        {
+            if (compChoice.equals("R"))
+            {
+                winner="player wins";
+            }
+            if (compChoice.equals("P"))
+            {
+                winner="tie";
+            }
+            if (compChoice.equals("S"))
+            {
+                winner="computer wins";
+            }
+        }
+
+        if (playChoice.equals("S"))
+        {
+            if (compChoice.equals("R"))
+            {
+                winner="computer wins";
+            }
+            if (compChoice.equals("P"))
+            {
+                winner="player wins";
+            }
+            if (compChoice.equals("S"))
+            {
+                winner="tie";
+            }
+        }
+
 		return winner;
 	}
 
 	public String toString()
 	{
-		String output="";
-		return output;
+		String output=determineWinner();
+        return (String)("Computer had "+compChoice+" and Player had "+playChoice+". "+output);
 	}
 }
